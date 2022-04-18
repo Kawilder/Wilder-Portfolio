29ef90c845565c9a8f5f07f98dda7b364d4b8c75
Kawilder <kevin_wilde564@yahoo.com>
2022-04-17 20:06:40 -0500

    filled out the portfolio file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
 import './App.css';
 //import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Nav from './components/Nav';
 import Contact from './components/Contact';
-import Project from './components/Portfolio';
+import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
+import React, { useState } from 'react';
   
 function App() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -16,7 +16,7 @@ function App() {
       return <About />;
     }
     if (currentPage === 'Project') {
-      return <Project />;
+      return <Portfolio />;
     }
     if (currentPage === 'Resume') {
       return <Resume />;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from '../Project';
+
+function Portfolio() {
+    return (
+        <section>
+            <h1 className="section-title portfolio-title">My Portfolio</h1>
+            <Project />
+        </section>
+    );
+}
+
+export default Portfolio;
